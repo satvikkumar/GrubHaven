d34fab41ef60feacd67c8907d9665ec81054c4a2
Shikhar97 <shikharprakhar@gmail.com>
2018-10-13 15:22:10 +0530

    Added link to pages

--- Frontend/src/pages/home/home.ts
+++ Frontend/src/pages/home/home.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ListPage } from '../list/list';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  list=ListPage
   constructor(public navCtrl: NavController) {
 
   }
