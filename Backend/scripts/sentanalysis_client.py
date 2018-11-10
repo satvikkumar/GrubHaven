b73823e177068ac4a7d4acffd43c7448add3c505
pshreyasv100 <pshreyasv@gmail.com>
2018-11-11 00:33:29 +0530

    Modified reviews sentanalysis

--- Backend/scripts/sentanalysis_client.py
+++ Backend/scripts/sentanalysis_client.py
@@ -1,11 +1,12 @@
 # importing the requests library 
 import requests
 import nltk 
+from itertools import groupby
 
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 # api-endpoint 
-URL = "http://18.136.208.244:8080/api/viewReviewByRestaurant"
+URL = "http://18.136.208.244:8080/api/viewReview"
 
 analyser = SentimentIntensityAnalyzer()
 
@@ -13,9 +14,9 @@ def _sent_scores(sentence):
     snt = analyser.polarity_scores(sentence)
     return snt
     
-
 def getSentiment(review):
 	sentences = nltk.sent_tokenize(review)
+
 	for sent in sentences:
 		score = _sent_scores(sent)
 		pos=0
@@ -24,32 +25,30 @@ def getSentiment(review):
 		pos += score['pos']
 		neg += score['neg']
 		neut += score['neu']
-		length = len(sentences)
+	length = len(sentences)
 	pos = 10*pos/length
 	neg = 10*neg/length
 	neut = 10*neut/length
 
-	sentiment = {'positive' : pos, 'negative' :neg, 'neutral':neut}
+	if pos > neg and pos > neut:
+		sentiment = "positive"	
+	elif neg > pos and neg > neut:
+		sentiment = "negative"
+	else:
+		sentiment = "neutral"
+	
 	return sentiment
 
-hotel_names = ["7Star","AKF","BBA"]
-hotel_data = []
+res = requests.post(url = URL ,data = {"city":"Bangalore"})
 
-#Retrieve data from database of manager side
-for hotel in hotel_names:
-	r = requests.post(url = URL,data={'name':hotel}) 
-	data = r.json()
-	hotel_data.append(data)
-
-print(hotel_data)
+print(res)
+review_data  = res.json()
 
 
 #Getting the sentiment for each review 
 #For now it just prints the positive,negative ,neutral sentiments of review
-for data in hotel_data:
-	for item in data:
-		print(item['hotel_name']," review by ", item['customer_name'])
-
-
-		print(getSentiment(item['review']))
-		#print(item['customer_name'])
+for item in review_data:
+	print("review",item['review'])
+	print(item['hotel_name']," review by ", item['customer_name'])
+	print(getSentiment(item['review']))
+	print()
