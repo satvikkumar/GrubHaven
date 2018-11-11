370dac9501e6beb92fbe979d693a5b84ce1f594a
pshreyasv100 <pshreyasv@gmail.com>
2018-11-11 11:21:07 +0530

    Updated sentanalysis to dump scores in /Backend/assets

--- Backend/scripts/sentanalysis_client.py
+++ Backend/scripts/sentanalysis_client.py
@@ -1,7 +1,11 @@
 # importing the requests library 
 import requests
 import nltk 
+import os
 from itertools import groupby
+from collections import defaultdict
+
+
 
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
@@ -26,21 +30,36 @@ def getSentiment(review):
 		neg += score['neg']
 	length = len(sentences)
 	pos = 10*pos/length
-	neg = 10*neg/length
+	neg = - 10*neg/length
 
-
-	return "positive" if pos > neg else "negative"
+	return pos if pos > neg else neg
 
 res = requests.post(url = URL ,data = {"city":"Bangalore"})
-
-print(res)
+#print(res)
 review_data  = res.json()
 
 
+hotel_sentscore = defaultdict(list)
+
+
 #Getting the sentiment for each review 
-#For now it just prints the positive,negative ,neutral sentiments of review
 for item in review_data:
-	print("review",item['review'])
-	print(item['hotel_name']," review by ", item['customer_name'])
-	print(getSentiment(item['review']))
-	print()
+	score = getSentiment(item['review'])
+	hotel_sentscore[item['hotel_name']].append(score)
+
+
+cur_dir = os.path.dirname(os.path.realpath('sentanalysis_client.py'))
+print(cur_dir)
+
+target_dir = os.path.join(cur_dir, '../assets/')
+target_dir = os.path.abspath(os.path.realpath(target_dir))
+os.chdir(target_dir)
+
+#Aggregating sentiment score for a hotel and creating a file for each hotel with respective score
+for hotel in hotel_sentscore.keys():
+	avg_score = sum(hotel_sentscore[hotel])/(len(hotel_sentscore[hotel]))
+	
+	fname = str(hotel) + "_sentiment.txt"
+	file  = open(fname,'a')
+	file.write(str(avg_score))
+	
