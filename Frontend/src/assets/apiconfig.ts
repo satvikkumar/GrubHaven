cad8a1d0bf9ad7cb0ab515673ad14186af0677b0
pshreyasv100 <pshreyasv@gmail.com>
2018-11-16 06:40:38 +0530

    bug fixes

--- Frontend/src/assets/apiconfig.ts
+++ Frontend/src/assets/apiconfig.ts
@@ -1,8 +1,4 @@
 export enum APIURL{
     URL1 = "http://18.136.208.244:8080"
-<<<<<<< HEAD
-    // URL1 = "http://localhost:8080"
-=======
     //URL1 = "http://localhost:8080"
->>>>>>> 6986c7719f0e3e624492fde9c5a3b3cd8ece9630
 }
